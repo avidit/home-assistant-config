b66736bc92041752f225a6a127f997cf7dcbc1ac
avidit <aabishkar@gmail.com>
2020-03-14 11:13:57 -0400

    :sparkles: Update homeassistant ttp 0.107 beta
    Update lovelace config backup script
    Modify lovelace config
    Update hacs plugins

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hass
+# home assistant
 
 my home automation config
 
--- scripts/save_lovelace_config.py
+++ scripts/save_lovelace_config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import json
 from ruamel.yaml import YAML
@@ -7,10 +8,23 @@ from ruamel.yaml import YAML
 yaml = YAML()
 yaml.indent(sequence=4, offset=2)
 
-try:
-    with open('.storage/lovelace') as json_file:
-        with open('ui-lovelace.yaml', 'w') as yaml_file:
-            config = json.load(json_file)['data']['config']
-            yaml.dump(config, yaml_file)
-except Exception as error:
-    print(f'failed to save config, {error}')
+
+def save(json_file):
+    """save lovelace json files in yaml format"""
+    try:
+        with open(json_file) as source_file:
+            yaml_file = os.path.join(
+                'lovelace', os.path.basename(json_file) + '.yaml')
+            os.makedirs(os.path.dirname(yaml_file), exist_ok=True)
+            with open(yaml_file, 'w') as target_file:
+                content = json.load(source_file)
+                yaml.dump(content, target_file)
+    except Exception as error:
+        print(f'failed to save config, {error}')
+
+
+rootdir = '.storage'
+for file in os.listdir(rootdir):
+    if file.startswith('lovelace'):
+        json_file = os.path.join(rootdir, file)
+        save(json_file)
